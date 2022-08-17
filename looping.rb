2d62cb31e66a104bd75b5ca30988844d4e683fc0
Zamzam <Zamzamhassan44@gmail.com>
2022-08-17 23:22:27 +0300

    Ruby flow loops

--- looping.rb
+++ looping.rb
@@ -1,8 +1,15 @@
 def happy_new_year
   # your code here
+  i = 10
+  until i == 0
+  puts i 
+  i -= 1
+  end
+  puts "Happy New Year!"
 end
 
-# No need to modify this code! Use this to implement the fizzbuzz_printer method.
+
+# # No need to modify this code! Use this to implement the fizzbuzz_printer method.
 def fizzbuzz(num)
   if num % 3 == 0 && num % 5 == 0
     "FizzBuzz"
@@ -17,8 +24,18 @@ end
 
 def fizzbuzz_printer
   # your code here
+  (1..100).each do |num|
+    puts fizzbuzz(num)
+  end
 end
 
-def reverse_string(str)
-  # your code here
+
+def reverse_string(hello)
+reversed_string = ""
+i = 0
+while i < hello.length
+  reversed_string = hello[i] + reversed_string
+  i += 1
+end
+reversed_string
 end
